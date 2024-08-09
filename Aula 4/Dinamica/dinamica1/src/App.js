ca3f5a4885f9710ac127d4096fbbc15e7979ea44
CarolineT9 <caroltvrs@gmail.com>
2024-08-08 21:37:37 -0300

    Aula 4

---
+++ Aula 4/Dinamica/dinamica1/src/App.js
@@ -0,0 +1,27 @@
+import {useState} from 'react'
+import {FaStar} from 'react-icons/fa'
+
+ function StarRating(nStarts) {
+  const [rate, setRate] = useState(0)
+  const starList = Array.from ({length: nStarts}, (_,k) => k+1)
+  function evaluate(star){
+    setRate(star)
+  }
+  return(
+    
+    <>{
+      starList.map(
+        (n) => <FaStar onClick={_=>evaluate(n)}
+        color={n<=rate ? 'gold' : 'lightgray'}
+        >
+
+        </FaStar>
+      )
+    }
+    <p>O rate atual é {rate}</p>
+    </>
+  )
+
+}
+
+export default StarRating;
